9e6e9810f8a988c185368afc95d8e498fed1a3da
DMosqueraL <dml2410@gmail.com>
2023-11-19 11:09:27 -0500

    Se implementa Swagger 3.0 en el proyecto

--- src/main/java/apiserviciotransporte/apiserviciotransporte/controladores/SolicitudServicioController.java
+++ src/main/java/apiserviciotransporte/apiserviciotransporte/controladores/SolicitudServicioController.java
@@ -5,9 +5,11 @@ import apiserviciotransporte.apiserviciotransporte.controladores.dto.SolicitudSe
 import apiserviciotransporte.apiserviciotransporte.controladores.dto.SolicitudesServicioResponseDto;
 import apiserviciotransporte.apiserviciotransporte.entidades.SolicitudServicio;
 import apiserviciotransporte.apiserviciotransporte.interfaces.SolicitudServicioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +17,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +30,8 @@ public class SolicitudServicioController {
 
     private final SolicitudServicioService solicitudServicio;
 
+    @Operation(summary = "- Endpoint para realizar una solicitud de servicio en SSMU")
+    @ApiResponse(responseCode = "200", description = "Solicitud Realizada con Éxito")
     @PostMapping(value = "", consumes = "application/json", produces = "application/json")
     public ResponseEntity<?> crearSolicitudServicio(@Valid @RequestBody SolicitudServicioDto solicitudServicioDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -40,18 +43,22 @@ public class SolicitudServicioController {
                 .body(solicitudGuardada);
     }
 
+    @Operation(summary = "- Endpoint para obtener el listado de solicitudes realizadas en SSMU")
+    @ApiResponse(responseCode = "200", description = "Listado de Solicitudes de Servicio")
     @GetMapping(value = "", produces = "application/json")
     public ResponseEntity<SolicitudesServicioResponseDto> listarSolicitudesDeServicio(
             @RequestParam("page") int page,
             @RequestParam("size") int size,
             @RequestParam("type") SolicitudServicioDto.TipoSolicitud type
-            ) {
+    ) {
         SolicitudesServicioResponseDto responseDto = this.solicitudServicio.listar(page, size, type);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(responseDto);
     }
 
+    @Operation(summary = "- Endpoint para obtener una solicitud de servicio en SSMU por su id")
+    @ApiResponse(responseCode = "200", description = "Operación Exitosa")
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<SolicitudServicioDto> obtener(@PathVariable long id) {
         SolicitudServicioDto solciitudDto = solicitudServicio.obtener(id);
@@ -60,7 +67,8 @@ public class SolicitudServicioController {
                 .body(solciitudDto);
     }
 
-
+    @Operation(summary = "- Endpoint para buscar una solicitud de servicio en SSMU por fecha")
+    @ApiResponse(responseCode = "200", description = "Solicitud de Servicio por fecha obtenida con éxito")
     @GetMapping(value = "/date/{fecha}", produces = "application/json")
     public ResponseEntity<List<SolicitudServicioDto>> buscarPorFecha(@PathVariable("fecha") LocalDateTime fecha) {
         List<SolicitudServicioDto> solicitudes = solicitudServicio.buscarPorFecha(fecha);
@@ -76,6 +84,8 @@ public class SolicitudServicioController {
 //        return servicioServicio.buscarPorUsuario(usuario.getUsuario());
 //    }
 
+    @Operation(summary = "- Endpoint para eliminar una solicitud de servicio en SSMU")
+    @ApiResponse(responseCode = "200", description = "Solicitud de Servicio eliminada con éxito")
     @DeleteMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<SolicitudDeleteResponseDto> eliminar(@PathVariable("id") long id) {
         boolean eliminado = solicitudServicio.eliminar(id);
@@ -87,6 +97,8 @@ public class SolicitudServicioController {
                 .body(solicitudDeleteResponseDto);
     }
 
+    @Operation(summary = "- Endpoint para actualizar una solicitud de servicio en SSMU")
+    @ApiResponse(responseCode = "200", description = "Solicitud de Servicio actualizada con éxito")
     @PutMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
     public SolicitudServicio actualizar(@PathVariable("id") String id, @RequestBody SolicitudServicioDto solicitudServicio) {
         return null;
@@ -120,4 +132,3 @@ public class SolicitudServicioController {
     }
 
 }
-
